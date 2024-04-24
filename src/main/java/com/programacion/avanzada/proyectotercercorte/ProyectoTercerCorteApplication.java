d6479a93ad07a8df3b6735c45fedd9b01c5d654c
Johann Rojas <johann.rojasu@gmail.com>
2024-04-24 18:35:18 -0500

    first commit

---
+++ src/main/java/com/programacion/avanzada/proyectotercercorte/ProyectoTercerCorteApplication.java
@@ -0,0 +1,13 @@
+package com.programacion.avanzada.proyectotercercorte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoTercerCorteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoTercerCorteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/programacion/avanzada/proyectotercercorte/ProyectoTercerCorteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programacion.avanzada.proyectotercercorte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoTercerCorteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
